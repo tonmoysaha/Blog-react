a3321feb52d7316b57685615d4b62874f1888553
tonmoy.saha <tonmoy.saha@bracits.com>
2023-12-22 14:01:17 +0600

    fix link with layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Layout from "./pages/header-router-layout";
+import Header from "./pages/header";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 import React from "react";
@@ -9,7 +9,7 @@ function App() {
         <div>
             <nav className="bg-blue-500 p-4 text-white">
                 <BrowserRouter>
-                    <Layout></Layout>
+                    <Header></Header>
                     <Routes>
                         <Route path="/" element={<Home/>}></Route>
                         <Route path="/home" element={<Home/>}></Route>
--- src/pages/header.js
+++ src/pages/header.js
@@ -1,37 +1,31 @@
+import {Link, Outlet} from "react-router-dom";
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Contact from "./contact";
-import Home from "./home";
-import Layout from "./header-router-layout";
+
 
 const Header = () => {
     return (
         <div>
-            {/*<nav className="bg-blue-500 p-4 text-white">*/}
-            {/*    <BrowserRouter>*/}
-            {/*        <Layout></Layout>*/}
-            {/*        <div className="container mx-auto flex justify-between items-center">*/}
-            {/*            <Routes>*/}
-            {/*                <Route path="/" element={<Home/>} className="text-2xl font-bold"></Route>*/}
-            {/*            </Routes>*/}
-            {/*            <ul className="flex space-x-4">*/}
-            {/*                <li>*/}
-            {/*                    <Routes>*/}
-            {/*                        <Route path="/home" element={<Home/>} className="hover:text-gray-300"></Route>*/}
-            {/*                    </Routes>*/}
-            {/*                </li>*/}
-            {/*                <li>*/}
-            {/*                    <Routes>*/}
-            {/*                        <Route path="/contact" element={<Contact></Contact>}*/}
-            {/*                               className="hover:text-gray-300"></Route>*/}
-            {/*                    </Routes>*/}
-            {/*                </li>*/}
-            {/*            </ul>*/}
-            {/*        </div>*/}
-            {/*    </BrowserRouter>*/}
-            {/*</nav>*/}
+            <nav className="bg-blue-500 p-4 text-white">
+                <div className="container mx-auto flex justify-between items-center">
+
+                    <Link to="/" className="text-2xl font-bold">My Blog</Link>
+
+                    <ul className="flex space-x-4">
+                        <li>
+
+                            <Link to="/home" className="hover:text-gray-300">Home</Link>
+
+                        </li>
+                        <li>
+                            <Link to="/contact"
+                                  className="hover:text-gray-300">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-    );
-}
+    )
+};
+
 
 export default Header
