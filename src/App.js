086dee85910df9028b900deb140df37c4b80c5c0
tonmoy.saha <tonmoy.saha@bracits.com>
2023-12-22 13:20:57 +0600

    add fix route mapping route mapping

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import Home from './pages/home'
-import Contact from './pages/contact'
-import {BrowserRouter, Link, Outlet, Route, Router, Routes} from "react-router-dom";
-
-const Layout = () => {
-    return (
-        <>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-
-            <Outlet />
-        </>
-    )
-};
+import Header from './pages/header'
+import {BrowserRouter as Route, Router, Routes} from "react-router-dom";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="/contact" element={<Contact></Contact>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 text-center">
+            <Header></Header>
+        </div>
     );
 }
 
---
+++ src/pages/header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Contact from "./contact";
+import Home from "./home";
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="bg-blue-500 p-4 text-white">
+                <BrowserRouter>
+                    <div className="container mx-auto flex justify-between items-center">
+                        <Routes>
+                            <Route path="/" element={<Home/>} className="text-2xl font-bold"></Route>
+                        </Routes>
+                        <ul className="flex space-x-4">
+                            <li>
+                                <Routes>
+                                    <Route path="/home" element={<Home/>} className="hover:text-gray-300"></Route>
+                                </Routes>
+                            </li>
+                            <li>
+                                <Routes>
+                                    <Route path="/contact" element={<Contact></Contact>}
+                                           className="hover:text-gray-300"></Route>
+                                </Routes>
+                            </li>
+                        </ul>
+                    </div>
+                </BrowserRouter>
+            </nav>
+        </div>
+    );
+}
+
+export default Header
